932ecf54327411033327bdfe4b321630ecf191fb
aliRahimi1997 <83342120+aliRahimi1997@users.noreply.github.com>
2026-09-08 01:37:44 +0330

    Update Utils for costem dgl

--- android/src/com/reecedunn/espeak/Utils.java
+++ android/src/com/reecedunn/espeak/Utils.java
@@ -1,26 +1,100 @@
 package com.reecedunn.espeak;
 
-import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
 import android.net.Uri;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AlertDialog;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Utils {
 
     public static void openTelegram(Context context) {
         Intent telegramIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=r2998"));
-        Intent chooser = Intent.createChooser(telegramIntent, context.getString(R.string.chooser_telegram));
+        PackageManager pm = context.getPackageManager();
 
-        try {
-            context.startActivity(chooser);
-        } catch (ActivityNotFoundException e1) {
-            Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/r2998"));
+        List<ResolveInfo> activities = pm.queryIntentActivities(telegramIntent, 0);
+
+        if (activities.isEmpty()) {
             try {
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/r2998"));
                 context.startActivity(webIntent);
-            } catch (ActivityNotFoundException e2) {
+            } catch (Exception e) {
                 Toast.makeText(context, context.getString(R.string.no_telegram_app), Toast.LENGTH_LONG).show();
             }
+            return;
+        }
+
+        if (activities.size() == 1) {
+            ResolveInfo info = activities.get(0);
+            telegramIntent.setClassName(info.activityInfo.packageName, info.activityInfo.name);
+            context.startActivity(telegramIntent);
+            return;
+        }
+
+        List<Object> items = new ArrayList<>(activities);
+        items.add("Cancel");
+
+        ArrayAdapter<Object> adapter = new ArrayAdapter<Object>(context, android.R.layout.select_dialog_item, items) {
+            @NonNull
+            @Override
+            public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+                TextView textView = (TextView) super.getView(position, convertView, parent);
+                Object item = getItem(position);
+
+                if (item instanceof ResolveInfo) {
+                    ResolveInfo info = (ResolveInfo) item;
+                    textView.setText(info.loadLabel(pm));
+                    
+                    int iconSize = (int) (24 * context.getResources().getDisplayMetrics().density);
+                    android.graphics.drawable.Drawable icon = info.loadIcon(pm);
+                    icon.setBounds(0, 0, iconSize, iconSize);
+                    textView.setCompoundDrawables(icon, null, null, null);
+                    textView.setCompoundDrawablePadding((int) (12 * context.getResources().getDisplayMetrics().density));
+                } else if (item instanceof String) {
+                    textView.setText((String) item);
+                    textView.setCompoundDrawables(null, null, null, null);
+                }
+
+                return textView;
+            }
+        };
+
+        AlertDialog dialog = new AlertDialog.Builder(context)
+                .setTitle(context.getString(R.string.chooser_telegram))
+                .setAdapter(adapter, (dialogInterface, i) -> {
+                    Object selectedItem = items.get(i);
+                    if (selectedItem instanceof ResolveInfo) {
+                        ResolveInfo info = (ResolveInfo) selectedItem;
+                        Intent targetIntent = new Intent(telegramIntent);
+                        targetIntent.setClassName(info.activityInfo.packageName, info.activityInfo.name);
+                        context.startActivity(targetIntent);
+                    } else {
+                        dialogInterface.dismiss();
+                    }
+                })
+                .create();
+
+        dialog.show();
+
+        if (dialog.getListView() != null) {
+            dialog.getListView().post(() -> {
+                dialog.getListView().requestFocus();
+                if (dialog.getListView().getChildAt(0) != null) {
+                    dialog.getListView().getChildAt(0).sendAccessibilityEvent(android.view.accessibility.AccessibilityEvent.TYPE_VIEW_FOCUSED);
+                }
+            });
         }
     }
 }

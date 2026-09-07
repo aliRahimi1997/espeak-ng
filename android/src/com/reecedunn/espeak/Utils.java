42e6cda202c604648ae2bb3b24e6c43587293ee3
aliRahimi1997 <83342120+aliRahimi1997@users.noreply.github.com>
2026-09-08 00:18:37 +0330

    update utilsAgain

--- android/src/com/reecedunn/espeak/Utils.java
+++ android/src/com/reecedunn/espeak/Utils.java
@@ -9,13 +9,24 @@ import android.widget.Toast;
 public class Utils {
 
     public static void openTelegram(Context context) {
-        Intent telegramIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=r2998"));
-        PackageManager packageManager = context.getPackageManager();
-        if (telegramIntent.resolveActivity(packageManager) != null) {
-            Intent chooser = Intent.createChooser(telegramIntent, context.getString(R.string.chooser_telegram));
-            context.startActivity(chooser);
-        } else {
+        PackageManager pm = context.getPackageManager();
+
+        try {
+            pm.getPackageInfo("org.telegram.messenger", 0);
+
+            Intent telegramIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("tg://resolve?domain=r2998"));
+
+            if (telegramIntent.resolveActivity(pm) != null) {
+                Intent chooser = Intent.createChooser(telegramIntent, context.getString(R.string.chooser_telegram));
+                context.startActivity(chooser);
+            } else {
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("https://t.me/r2998"));
+                context.startActivity(webIntent);
+            }
+
+        } catch (PackageManager.NameNotFoundException e) {
             Toast.makeText(context, context.getString(R.string.no_telegram_app), Toast.LENGTH_LONG).show();
+
             try {
                 Intent marketIntent = new Intent(Intent.ACTION_VIEW, Uri.parse("market://details?id=org.telegram.messenger"));
                 context.startActivity(marketIntent);

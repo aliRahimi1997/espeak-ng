3a20dba66f814bc0368e91ab9c6b0f7d0e28934a
alirahimi1997 <ali.c5468@gmail.com>
2026-09-11 17:53:33 +0330

    ee932Aslaia

--- cmake/data.cmake
+++ cmake/data.cmake
@@ -1,28 +1,6 @@
 list(APPEND _dict_compile_list
-  ab af am an ar as az
-  ba be bg bn bpy bs
-  ca chr cmn crh cs cv cy
-  da de
-  el en eo es et eu
-  fa fi fo fr
-  ga gd gn grc gu
-  hak haw he hi hr ht hu hy
-  ia id io is it
-  ja jbo
-  ka kaa kk kl kn kok ko ku ky
-  la lb lfn lij lt lv
-  mi mk ml mn mr ms mto mt my
-  nci ne nl nog no
-  om or
-  pap pa piqd pl ps pt py
-  qdb quc qu qya
-  ro ru rup
-  sd shn si sjn sk sl smj sq sr sv sw
-  ta te ti th tk tn tr tt
-  ug uk ur uz
-  vi
-  xex
-  yue
+  en
+  fa
 )
 
 list(APPEND _mbrola_lang_list

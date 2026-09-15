206e7f83fb740b0b2eafa480d6eb3cb32dc87a2b
alirahimi1997 <ali.c5468@gmail.com>
2026-09-16 01:37:12 +0330

    hejalsa

--- android/src/com/reecedunn/espeak/EspeakApp.java
+++ android/src/com/reecedunn/espeak/EspeakApp.java
@@ -2,7 +2,11 @@ package com.reecedunn.espeak;
 
 import android.app.Application;
 import android.content.Context;
+import android.content.res.Configuration;
 import android.os.Build;
+import android.os.LocaleList;
+
+import java.util.Locale;
 
 public class EspeakApp extends Application {
 
@@ -17,6 +21,28 @@ public class EspeakApp extends Application {
         } else {
             EspeakApp.storageContext = appContext;
         }
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        Locale systemLocale = getSystemLocale();
+        Configuration config = new Configuration(base.getResources().getConfiguration());
+        config.setLocale(systemLocale);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            config.setLocales(new LocaleList(systemLocale));
+        }
+
+        Context localized = base.createConfigurationContext(config);
+        super.attachBaseContext(localized);
+    }
+    private static Locale getSystemLocale() {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.N) {
+            LocaleList list = LocaleList.getDefault();
+            if (list != null && !list.isEmpty()) {
+                return list.get(0);
+            }
+        }
+        return Locale.getDefault();
     }
 
     public static Context getStorageContext() {

d5bb0e77d02dbf04b8e298438be74c96002925f5
alirahimi1997 <ali.c5468@gmail.com>
2026-09-17 02:46:42 +0330

    peerrvldasa

--- android/src/com/reecedunn/espeak/TtsService.java
+++ android/src/com/reecedunn/espeak/TtsService.java
@@ -342,9 +342,15 @@ public class TtsService extends TextToSpeechService {
 private String filterPersianDates(String text) {
     if (text == null || text.isEmpty()) return text;
     
-    String regex = "([0-9\\u0660-\\u0669\\u06F0-\\u06F9]+)-([0-9\\u0660-\\u0669\\u06F0-\\u06F9]+)-([0-9\\u0660-\\u0669\\u06F0-\\u06F9]+)";
+    text = text.replaceAll("(?<=[0-9\\u0660-\\u0669\\u06F0-\\u06F9])-(?=[0-9\\u0660-\\u0669\\u06F0-\\u06F9])", " ");
     
-    return text.replaceAll(regex, "$1 $2 $3");
+    text = text.replaceAll("(?<=[a-zA-Z\\u0600-\\u06FF])-(?=[0-9\\u0660-\\u0669\\u06F0-\\u06F9])", " ");
+    
+    text = text.replaceAll("(?<=[0-9\\u0660-\\u0669\\u06F0-\\u06F9])-(?=[a-zA-Z\\u0600-\\u06FF])", " ");
+    
+    text = text.replaceAll("(?<=[a-zA-Z\\u0600-\\u06FF])-(?=\\s|$)", "");
+    
+    return text;
 }
     @Override
     protected synchronized void onSynthesizeText(SynthesisRequest request, SynthesisCallback callback) {

2fc01f1382ac9e0ce039363ef47e8500fc624e89
alirahimi1997 <ali.c5468@gmail.com>
2026-09-17 12:48:46 +0330

    lieel

--- android/src/com/reecedunn/espeak/TtsService.java
+++ android/src/com/reecedunn/espeak/TtsService.java
@@ -342,6 +342,10 @@ public class TtsService extends TextToSpeechService {
 private String filterPersianDates(String text) {
     if (text == null || text.isEmpty()) return text;
     
+    if (text.trim().equals("\u0648")) {
+        return "\u0648\u0627\u0648";
+    }
+    
     text = text.replaceAll("(?<=[0-9\\u0660-\\u0669\\u06F0-\\u06F9])-(?=[0-9\\u0660-\\u0669\\u06F0-\\u06F9])", " ");
     
     text = text.replaceAll("(?<=[a-zA-Z\\u0600-\\u06FF])-(?=[0-9\\u0660-\\u0669\\u06F0-\\u06F9])", " ");

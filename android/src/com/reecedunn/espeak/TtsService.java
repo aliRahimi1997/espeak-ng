2c4d4b0d030686a259d3b8a3c9f5adf62f271b45
alirahimi1997 <ali.c5468@gmail.com>
2026-09-24 15:16:52 +0330

    plllll1

--- android/src/com/reecedunn/espeak/TtsService.java
+++ android/src/com/reecedunn/espeak/TtsService.java
@@ -346,6 +346,7 @@ public class TtsService extends TextToSpeechService {
                 text = "\u0648\u0627\u0648";
             } else {
                 text = text.replaceAll("(?<![0-9\\u0660-\\u0669\\u06F0-\\u06F9])[-−](?=\\s*[0-9\\u0660-\\u0669\\u06F0-\\u06F9])", " \u0645\u0646\u0641\u06CC\u0647 ");
+text = text.replaceAll("(?<=[0-9\\u0660-\\u0669\\u06F0-\\u06F9])-(?![0-9\\u0660-\\u0669\\u06F0-\\u06F9])", " ");
                 text = text.replaceAll("(?<=\\s|^)\u200C|\u200C(?=\\s|$)", "");
             }
         }
